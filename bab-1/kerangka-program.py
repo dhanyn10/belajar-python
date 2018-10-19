a0459bf1bdf6bd25cec78efd18572bcfdd9e001a
dhanyn10 <dhanyn.dhan@gmail.com>
2018-10-19 13:53:14 +0700

    program python dalam bentuk kerangka program

---
+++ bab-1/kerangka-program.py
@@ -0,0 +1,12 @@
+from __future__ import print_function
+import os
+
+#mendefinisikan fungsi main()
+def main():
+    bahasa = ['JavaScript', 'Java', 'PHP', 'Python']
+    for a in bahasa:
+        print(a)
+    os.system("pause");
+
+if __name__ == "__main__":
+    main() #memanggil fungsi main()
