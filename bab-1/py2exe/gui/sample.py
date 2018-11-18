aba670f1fa494f17b8430edc1ce706ef20cc10a8
dhanyn10 <dhanyn.dhan@gmail.com>
2018-11-18 11:07:40 +0700

    python gui(windows) : failed

---
+++ bab-1/py2exe/gui/sample.py
@@ -0,0 +1,15 @@
+from __future__ import print_function
+import os
+import sys
+
+def main():
+    print("Contoh penggunaan library py2exe")
+    print("sys.path:")
+    print(sys.path)
+    print("sys.executable\t: %s" % sys.executable)
+    print("sys.prefix\t: %s" % sys.prefix)
+    print("sys.argv\t: %s" %sys.argv)
+    os.system("pause")
+
+if __name__ == "__main__":
+    main()
---
+++ bab-1/py2exe/gui/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+import py2exe
+
+setup(
+    #tiga parameter di bawah ini bersifat opsional
+    version     = "0.1.0",
+    description = "contoh penggunaan py2exe",
+    name        = "sample",
+
+    #nama file yang akan dijadikan .exe
+    #program dalam bentuk console
+    windows     = ["sample.py"],
+)
