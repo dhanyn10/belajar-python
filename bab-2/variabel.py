fcca036d54117f316b2cc66a4a8df9219b05eedf
dhanyn10 <dhanyn.dhan@gmail.com>
2018-11-18 11:22:03 +0700

    variabel python

---
+++ bab-2/variabel.py
@@ -0,0 +1,19 @@
+from __future__ import print_function
+
+def main():
+    #membuat variabel
+    a = 10
+    b = 3.12
+    c = a * b
+    bahasa  = "python"
+    kalimat = "pemrograman python"
+
+    #menampilkan nilai variabel ke layar
+    print(a)
+    print(b)
+    print(c)
+    print(bahasa)
+    print(kalimat)
+
+if __name__ == "__main__":
+    main()
