60e539e2849c0a3e3e28dd32fa2348e0a911194f
dhanyn10 <dhanyn.dhan@gmail.com>
2018-11-18 11:28:30 +0700

    membaca data teks dan menampilkannya

---
+++ bab-2/input-string.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+
+def main():
+    #membuat prompt input untuk tipe data string
+    nama = raw_input("masukkan nama kamu: ")
+
+    #menampilkan output string "nama"
+    print("nama kamu : ", nama)
+
+if __name__ == "__main__":
+    main()
