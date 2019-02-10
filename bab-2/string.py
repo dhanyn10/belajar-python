8ddd200704dc772202018c49b33612c27582769d
dhanyn10 <dhanyn.dhan@gmail.com>
2019-02-10 10:15:09 +0700

    string

---
+++ bab-2/string.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+
+def main():
+    #membuat string
+    s1 = 'string pertama'
+    s2 = 'string kedua'
+
+    #menampilkan string
+    print(s1)
+    print(s2)
+
+if __name__ == "__main__":
+    main()
