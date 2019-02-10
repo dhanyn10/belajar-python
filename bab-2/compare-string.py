3df5bad4145f3baabb6c222d697e91da746e2a95
dhanyn10 <dhanyn.dhan@gmail.com>
2019-02-10 10:25:47 +0700

    perbandingan string dengan escapse yang berbeda

---
+++ bab-2/compare-string.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+
+def main():
+    s1 = 'python'
+    s2 = "python"
+
+    print("s1 : " + s1)
+    print("s2 : " + s2)
+
+    if s1 == s2:
+        print("s1 = s2")
+    else:
+        print("s1 != s2")
+
+if __name__ == "__main__":
+    main()
