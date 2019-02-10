b0775d6254a0fc8ee9799bbebe889b0f3ed12b4d
dhanyn10 <dhanyn.dhan@gmail.com>
2019-02-10 10:20:19 +0700

    penggunaa petik tunggal dan ganda

---
+++ bab-2/escape.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+
+def main():
+    s1 = 'petik tunggal \'escape\', petik ganda "OK"'
+    s2 = "petik tunggal 'OK', petik ganda \"escape\""
+    s3 = "baris pertama \n baris kedua"
+
+    print(s1)
+    print(s2)
+    print(s3)
+
+if __name__ == "__main__":
+    main()
